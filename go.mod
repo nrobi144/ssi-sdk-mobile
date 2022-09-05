480b714a731135575c271b45ed7d51fc0dfce835
Róbert Nagy <robert.nagy.dev@gmail.com>
2022-09-05 13:56:02 +0300

    Initialize repository

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/nrobi144/ssi-sdk-mobile
+
+require (
+	github.com/magefile/mage v1.13.0
+	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90
+)
+
+require (
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1 // indirect
+)
+
+go 1.18
---
+++ magefile.go
@@ -0,0 +1,233 @@
+//go:build mage
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"os/user"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"syscall"
+
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+const (
+	Go = "go"
+)
+
+// Build all mobile targets.
+func Build() {
+	fmt.Println("Building...")
+	IOS()
+	Android()
+}
+
+// Clean deletes any build artifacts.
+func Clean() {
+	fmt.Println("Cleaning...")
+	os.RemoveAll("bin")
+}
+
+// Test runs unit tests without coverage.
+// The mage `-v` option will trigger a verbose output of the test
+func Test() error {
+	return runTests()
+}
+
+func runTests(extraTestArgs ...string) error {
+	args := []string{"test"}
+	if mg.Verbose() {
+		args = append(args, "-v")
+	}
+	args = append(args, "-race", "-tags=jwx_es256k")
+	args = append(args, extraTestArgs...)
+	args = append(args, "./...")
+	testEnv := map[string]string{
+		"CGO_ENABLED": "1",
+		"GO111MODULE": "on",
+	}
+	writer := ColorizeTestStdout()
+	fmt.Printf("%+v", args)
+	_, err := sh.Exec(testEnv, writer, os.Stderr, Go, args...)
+	return err
+}
+
+func ColorizeTestOutput(w io.Writer) io.Writer {
+	writer := NewRegexpWriter(w, `PASS.*`, "\033[32m$0\033[0m")
+	return NewRegexpWriter(writer, `FAIL.*`, "\033[31m$0\033[0m")
+}
+
+func ColorizeTestStdout() io.Writer {
+	if terminal.IsTerminal(syscall.Stdout) {
+		return ColorizeTestOutput(os.Stdout)
+	}
+	return os.Stdout
+}
+
+type regexpWriter struct {
+	inner io.Writer
+	re    *regexp.Regexp
+	repl  []byte
+}
+
+func NewRegexpWriter(inner io.Writer, re string, repl string) io.Writer {
+	return &regexpWriter{inner, regexp.MustCompile(re), []byte(repl)}
+}
+
+func (w *regexpWriter) Write(p []byte) (int, error) {
+	r := w.re.ReplaceAll(p, w.repl)
+	n, err := w.inner.Write(r)
+	if n > len(r) {
+		n = len(r)
+	}
+	return n, err
+}
+
+func runGo(cmd string, args ...string) error {
+	return sh.Run(findOnPathOrGoPath("go"), append([]string{"run", cmd}, args...)...)
+}
+
+// InstallIfNotPresent installs a go based tool (if not already installed)
+func installIfNotPresent(execName, goPackage string) error {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	pathOfExec := findOnPathOrGoPath(execName)
+	if len(pathOfExec) == 0 {
+		cmd := exec.Command(Go, "get", "-u", goPackage)
+		cmd.Dir = usr.HomeDir
+		if err := cmd.Start(); err != nil {
+			return err
+		}
+		return cmd.Wait()
+	}
+	return nil
+}
+
+func findOnPathOrGoPath(execName string) string {
+	if p := findOnPath(execName); p != "" {
+		return p
+	}
+	p := filepath.Join(goPath(), "bin", execName)
+	if _, err := os.Stat(p); err == nil {
+		return p
+	}
+	fmt.Printf("Could not find %s on PATH or in GOPATH/bin\n", execName)
+	return ""
+}
+
+func findOnPath(execName string) string {
+	pathEnv := os.Getenv("PATH")
+	pathDirectories := strings.Split(pathEnv, string(os.PathListSeparator))
+	for _, pathDirectory := range pathDirectories {
+		possible := filepath.Join(pathDirectory, execName)
+		stat, err := os.Stat(possible)
+		if err == nil || os.IsExist(err) {
+			if (stat.Mode() & 0111) != 0 {
+				return possible
+			}
+		}
+	}
+	return ""
+}
+
+func goPath() string {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+		return ""
+	}
+	goPath, goPathSet := os.LookupEnv("GOPATH")
+	if !goPathSet {
+		goPath = filepath.Join(usr.HomeDir, Go)
+	}
+	return goPath
+}
+
+// CBT runs clean; build; test
+func CBT() error {
+	Clean()
+	if err := Build(); err != nil {
+		return err
+	}
+	if err := Test(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// CITest runs unit tests with coverage as a part of CI.
+// The mage `-v` option will trigger a verbose output of the test
+func CITest() error {
+	return runCITests()
+}
+
+func runCITests(extraTestArgs ...string) error {
+	args := []string{"test"}
+	if mg.Verbose() {
+		args = append(args, "-v")
+	}
+	args = append(args, "-tags=jwx_es256k")
+	args = append(args, "-covermode=atomic")
+	args = append(args, "-coverprofile=coverage.out")
+	args = append(args, extraTestArgs...)
+	args = append(args, "./...")
+	testEnv := map[string]string{
+		"CGO_ENABLED": "1",
+		"GO111MODULE": "on",
+	}
+	writer := ColorizeTestStdout()
+	fmt.Printf("%+v", args)
+	_, err := sh.Exec(testEnv, writer, os.Stderr, Go, args...)
+	return err
+}
+
+func installGoMobileIfNotPresent() error {
+	return installIfNotPresent("gomobile", "golang.org/x/mobile/cmd/gomobile@latest")
+}
+
+func Mobile() {
+	IOS()
+	Android()
+}
+
+// Generates the iOS packages
+// Note: this command also installs "gomobile" if not present
+func IOS() {
+	installGoMobileIfNotPresent()
+
+	fmt.Println("Building iOS...")
+	bindIOs := sh.RunCmd("gomobile", "bind", "-target", "ios")
+	fmt.Println("Building crypto package...")
+	bindIOs("crypto")
+	fmt.Println("Building did package...")
+	bindIOs("did")
+	fmt.Println("Building cryptosuite package...")
+	bindIOs("cryptosuite")
+}
+
+// Generates the Android packages
+// Note: this command also installs "gomobile" if not present
+func Android() {
+	installGoMobileIfNotPresent()
+
+	apiLevel := "23"
+	fmt.Println("Building Android - Api Level: " + apiLevel + "...")
+	bindAndroid := sh.RunCmd("gomobile", "bind", "-target", "android", "-androidapi", "23")
+	fmt.Println("Building crypto package...")
+	bindAndroid("crypto")
+	fmt.Println("Building did package...")
+	bindAndroid("did")
+	fmt.Println("Building cryptosuite package...")
+	bindAndroid("cryptosuite")
+}
